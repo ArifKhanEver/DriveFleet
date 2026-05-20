6ca8e191d392a538cc0709bed295927e1df62ffa
arifkhanever <mdshafiqul0404@gmail.com>
2026-05-20 22:45:54 +0600

    Client side proxy added

--- src/proxy.js
+++ src/proxy.js
@@ -1,18 +1,19 @@
 import { NextResponse } from 'next/server';
+import { headers } from "next/headers";
+import { auth } from './lib/auth';
 
-export function proxy(request) {
-  const token = request.cookies.get('better-auth.session-token'); 
-  const { pathname } = request.nextUrl;
-
-  const privateRoutes = ['/add-car', '/my-bookings', '/my-added-cars'];
-
-  if (privateRoutes.includes(pathname) && !token) {
-    return NextResponse.redirect(new URL('/login', request.url));
+export async function proxy(request) {
+  const session = await auth.api.getSession({
+      headers: await headers() // you need to pass the headers object.
+  })
+ 
+  if (!session) {
+    return NextResponse.redirect(new URL('/auth/login', request.url));
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/add-car', '/my-bookings', '/my-added-cars'],
+  matcher: ['/add-car', '/my-bookings', '/my-added-cars','/cars/:path'],
 };
